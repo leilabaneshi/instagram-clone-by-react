db4a2bc9b08d936136dc813b67e64de82f74890f
Leila Baneshi <baneshi.leila@gmail.com>
2020-11-10 23:22:25 +0100

    set logo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'
+          alt='instagram-logo'
+        />
+
+      </div>
       <h1>instagram</h1>
     </div>
   );
